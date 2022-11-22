d740b34a8cef0fc0f80a7072c5fa8e06267e7bca
RenanDrnls <rdorneles64@gmail.com>
2022-11-22 17:28:59 -0300

    Added search for books AVAILABLES and UNAVAILABLES

--- src/controllers/Books/list-books.ts
+++ src/controllers/Books/list-books.ts
@@ -1,21 +1,35 @@
 import { Request, Response } from "express";
 import { prismaClient } from "../../database/prismaClient";
 
-interface ReqQuery {
-    booked?: number
+type BookStatus = 'AVAILABLE' | 'UNAVAILABLE';
+
+const noBooksFounded = (response: Response) => {
+    response.json({ message: "No books founded." }).status(200).end();
 }
 
-export const listBooksController = async (request: Request<ReqQuery>, response: Response) => {
+export const listBooksController = async (request: Request, response: Response) => {
     try{
-        if(!request.query.booked){
+        if(!request.query.status){
             const books = await prismaClient.book.findMany();
-            console.log(books);
+            if(books.length === 0){
+                return noBooksFounded(response);
+            }
+            return response.status(200).end();
+        }
+
+        const status: BookStatus = request.query.status as BookStatus;
+        const books = await prismaClient.book.findMany({
+            where: {
+                bookStatus: status
+            }
+        });
+
+        if(books.length !== 0){
             return response.json(books).status(200).end();
         }
-        console.log("Specific");
-        return response.status(200).end();
+        return noBooksFounded(response);
     } catch(error) {
         console.log(error);
-        return response.status(500).end();
+        return response.json({ message: "An error ocurred, please contact the administrator." }).status(500).end();
     }
 }
